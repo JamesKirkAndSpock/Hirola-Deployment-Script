dcfaa4ef460771536ebf35b0406b7109b1821e47
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-09-30 18:15:56 +0300

    feat(http-to-https): redirect to https
     - add letsencrypt certificates to application.
     - provide .env capability for running terraform commands
    - Install nginx on the base machine
    - refactor deployment script for easy readablility
    - serve the application with gunicorn
    - collect static files
    - copy nginx configuration to the VM
    
    [ Fixes #156068329 ]

--- deploy_hirola.sh
+++ deploy_hirola.sh
@@ -12,14 +12,10 @@ get_var() {
     "http://metadata.google.internal/computeMetadata/v1/instance/attributes/${name}"
 }
 
-install_and_start_repo () {
+get_required_variables () {
     BRANCH="$(get_var "circleBranch")" 
-    cd ~
-    virtualenv --python=python3 hi-venv
-    source ~/hi-venv/bin/activate
-    git clone -b ${BRANCH} https://github.com/JamesKirkAndSpock/Hirola
-    pip install -r ~/Hirola/hirola/requirements.txt
     ENVIRONMENT="$(get_var "environment")"
+    ENV_INSTANCE="$(get_var "env")"
     export IP_ADDRESS="$(get_var "ipAddress")"
     export HOST="$(get_var "host")"
     export DATABASE_NAME="$(get_var "databaseName")"
@@ -34,15 +30,40 @@ install_and_start_repo () {
     export CACHE_PORT="$(get_var "cachePort")"
     export TWILIO_ACCOUNT_SID="$(get_var "twilioSID")"
     export TWILIO_AUTH_TOKEN="$(get_var "twilioTOK")"
+}
+
+copy_lets_encrypt_credentials () {
+    sudo mkdir -p /etc/letsencrypt/renewal-hooks/deploy 
+    sudo gsutil -m rsync -d -r gs://teke-bucket/letsencrypt/letsencrypt-"${ENV_INSTANCE}" /etc/letsencrypt
+    sudo gsutil -m rsync -d -r gs://teke-bucket/nginx-configurations/"${ENV_INSTANCE}" /etc/nginx/conf.d
+    cd /etc/letsencrypt
+    sudo mkdir renewal-hooks/post renewal-hooks/pre
+    sudo chmod 700 accounts archive keys live accounts/acme-v02.api.letsencrypt.org 
+    sudo chmod 644 cli.ini options-ssl-nginx.conf ssl-dhparams.pem .updated-options-ssl-nginx-conf-digest.txt .updated-ssl-dhparams-pem-digest.txt csr/0000_csr-certbot.pem renewal/"${HOST}".conf
+    sudo chmod 755 csr renewal archive/"${HOST}" live/"${HOST}"
+    sudo chmod 600 keys/0000_key-certbot.pem
+    sudo chmod -R 755 renewal-hooks
+}
+install_and_start_repo () {
+    cd ~
+    virtualenv --python=python3 hi-venv
+    source ~/hi-venv/bin/activate
+    git clone -b ${BRANCH} https://github.com/JamesKirkAndSpock/Hirola
+    pip install -r ~/Hirola/hirola/requirements.txt
     python3 ~/Hirola/hirola/manage.py makemigrations front
     python3 ~/Hirola/hirola/manage.py migrate front
     python3 ~/Hirola/hirola/manage.py migrate
+    python3 ~/Hirola/hirola/manage.py collectstatic --no-input
     sudo systemctl start memcached
-    nohup python3 ~/Hirola/hirola/manage.py runserver 0:80 &
+    sudo nginx -s reload
+    cd ~/Hirola/hirola/
+    nohup gunicorn -b 0.0.0.0:8000 --error-logfile /var/log/hirola-error.log hirola.wsgi &
 }
 
 
 main () {
+    get_required_variables
+    copy_lets_encrypt_credentials
     install_and_start_repo
 }
 
