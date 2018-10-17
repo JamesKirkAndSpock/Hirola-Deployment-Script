250eae70e545ca98f3bb5334015b703a76fe45b5
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-10-17 09:33:17 +0300

    test

--- deploy_hirola.sh
+++ deploy_hirola.sh
@@ -61,7 +61,7 @@ install_and_start_repo () {
     python3 ~/Hirola/hirola/manage.py migrate front
     python3 ~/Hirola/hirola/manage.py migrate
     if [[ "${ENV_INSTANCE}" == "devops" ]]; then
-        gsutil cp gs://teke-bucket/application-data/user.json ~/Hirola/front/fixtures/
+        gsutil cp gs://teke-bucket/application-data/user.json ~/Hirola/hirola/front/fixtures
         python3 ~/Hirola/hirola/manage.py loaddata user.json
         python3 ~/Hirola/hirola/manage.py loaddata data.json
     fi
